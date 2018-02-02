8c4967d123921376ed920d5b1cae3b26bb2a36f9
MacUSER <macuser@MacADMINs-iMac.local>
2018-02-02 15:39:36 +0200

    xmpp connect and auth

--- My Budget/ViewControllers/LogInViewController.swift
+++ My Budget/ViewControllers/LogInViewController.swift
@@ -7,21 +7,54 @@
 //
 
 import UIKit
+import XMPPFramework
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     let overviewTabViewControllerIndex      = 0
     let setUpCashFlowTabViewControllerIndex = 1
     
+    var xmppStream: XMPPStream!
+    let hostName: String = "xmpp.ipay.eu"
+    let hostPort: UInt16 = 5222
+    
+    var password: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func streamConnect(jid: String){
+        xmppStream = XMPPStream()
+        self.xmppStream.hostName = hostName
+        self.xmppStream.hostPort = hostPort
+        xmppStream.addDelegate(self, delegateQueue: DispatchQueue.main)
+        xmppStream.myJID = XMPPJID(string: jid)
+        
+        do {
+            try xmppStream.connect(withTimeout: 30)
+        }
+        catch {
+            print("error occured in connecting")
+        }
+    }
+    
+    func streamAuth(password: String) {
+        
+        do {
+            try xmppStream.authenticate(withPassword: password)
+        }
+        catch {
+            print("catch")
+        }
     }
     
     func goToTabBar(){
@@ -36,9 +69,28 @@ class LogInViewController: UIViewController {
     }
     
     @IBAction func loginClicked(_ sender: UIButton) {
+      
+        guard let jid = emailTextField.text, let pass = passwordTextField.text else {
+            print("no email and/or pass")
+            return
+        }
+        password = pass
+        streamConnect(jid: jid)
+    }
+}
+
+extension LogInViewController: XMPPStreamDelegate {
+    func xmppStreamDidConnect(_ sender: XMPPStream) {
+        print("DidConnect")
+        streamAuth(password: password)
+    }
+    
+    func xmppStreamDidAuthenticate(_ sender: XMPPStream) {
+        print("DidAuthenticate")
         goToTabBar()
     }
     
-    
-
+    func xmppStreamDidDisconnect(_ sender: XMPPStream, withError error: Error?) {
+        print("DidDisconnect")
+    }
 }
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '11.0'
+
+target 'My Budget' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for My Budget
+  pod 'XMPPFramework', :git => "https://github.com/robbiehanson/XMPPFramework.git", :branch => 'master'
+
+
+  target 'My BudgetTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'My BudgetUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
